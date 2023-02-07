9d4dc6fd0267bd962f4c764c44838344369c1e83
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-02-07 09:41:28 +0100

    creat delete toast

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import styles from './movieList.module.css';
-import movieService from '../../apiService/movieService';
-import MovieCard from '../MovieCard/MovieCard';
+import styles from './movieList.module.css'
+import movieService from '../../apiService/movieService'
+import MovieCard from '../MovieCard/MovieCard'
+import DeleteToast from '../DeleteToast/DeleteToast'
 
 export default function MovieList() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([])
 
   useEffect(() => {
-    movieService.getAll().then((data) => setMovies(data));
-  }, []);
+    movieService.getAll().then((data) => setMovies(data))
+  }, [])
 
   const deleteById = (idToDelete) => {
-    let newData = movies.filter((movie) => movie.id !== idToDelete);
-    setMovies(newData);
-  };
+    let newData = movies.filter((movie) => movie.id !== idToDelete)
+    setMovies(newData)
+  }
 
   return (
     <div>
@@ -23,8 +24,9 @@ export default function MovieList() {
           <MovieCard key={item.id} item={item} deleteById={deleteById} />
         ))}
       </div>
+      <DeleteToast></DeleteToast>
     </div>
-  );
+  )
 }
 
 // <div>
