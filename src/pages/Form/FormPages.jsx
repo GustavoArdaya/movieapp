5118aae195fce877ecefc31bd1251a2a1a89797f
Daniel-gallegO <danielgallegolopez@outlook.com>
2023-02-08 11:48:40 +0100

    style to form

--- src/pages/Form/FormPages.jsx
+++ src/pages/Form/FormPages.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React from "react";
+import styles from "./formPages.module.css";
 
 export default function FormPages() {
   return (
     <div>
-      <form>
-        <div>
-          <img src='' alt='movie' />
+      <form className={styles.form}>
+        <div className={styles.formImgContainer}>
+          <img
+            className={styles.formImg}
+            src="https://pharmamex.com/images/default.png"
+            alt="movie"
+          />
         </div>
-        <section>
-          <input type='text' name='title' placeholder='movie name' />
-          <textarea type='textarea' name='img' placeholder='introduce url' />
-          <button type='submit'>Crear</button>
+        <section className={styles.textInputsContainer}>
+          <input
+            className={styles.nameInput}
+            type="text"
+            name="title"
+            placeholder="movie name"
+          />
+          <textarea
+            className={styles.urlInput}
+            type="textarea"
+            name="img"
+            placeholder="introduce url"
+          />
+          <button className={styles.submitButton} type="submit">
+            Crear
+          </button>
         </section>
       </form>
     </div>
