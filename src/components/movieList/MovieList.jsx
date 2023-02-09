e320bfcac98678f604e850db8f3bb6c09f04b2fd
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-02-09 16:07:33 +0100

    creat function edit

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-
 import styles from './movieList.module.css'
 import movieService from '../../apiService/movieService'
 import MovieCard from '../MovieCard/MovieCard'
@@ -10,11 +9,12 @@ import film_spinner from '../../assets/film_spinner.png'
 export default function MovieList() {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  //const [isDeleteMode, setIsDeleteMode] = useState(false)
 
   useEffect(() => {
     movieService.getAll().then((data) => {
-      setMovies(data);
-      setIsLoading(false);
+      setMovies(data)
+      setIsLoading(false)
     })
   }, [])
 
@@ -22,39 +22,47 @@ export default function MovieList() {
     let newData = movies.filter((movie) => movie.id !== idToDelete)
     setMovies(newData)
   }
-  let idToDelete = "";
-  const [isDeleteMode, setIsDeleteMode] = useState(false);
+  let idToDelete = ''
+  const [isDeleteMode, setIsDeleteMode] = useState(false)
 
   const showDeleteModal = (id) => {
-    setIsDeleteMode(true);
-    idToDelete = id;
+    setIsDeleteMode(true)
+    idToDelete = id
     console.log(idToDelete)
   }
 
   const hideDeleteModal = () => {
-    setIsDeleteMode(false);
-    
+    setIsDeleteMode(false)
   }
 
   return (
     <div>
-      <div className={ styles.spinnerContainer }>
-        {isLoading ?
-        <div> 
-          <img className={ styles.spinner } src={film_spinner}></img>
-          <p className={styles.loadingText}>Cargando...</p> 
-        </div>
-        :
-        ""}
+      <div className={styles.spinnerContainer}>
+        {isLoading ? (
+          <div>
+            <img className={styles.spinner} src={film_spinner}></img>
+            <p className={styles.loadingText}>Cargando...</p>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
       <div className={styles.movieList}>
         {movies.map((item) => (
-          <MovieCard key={item.id} item={item} showDeleteModal={showDeleteModal} />
+          <MovieCard
+            key={item.id}
+            item={item}
+            showDeleteModal={showDeleteModal}
+          />
         ))}
       </div>
       <div className={styles.popups}>
-      <DeleteModal isDeleteMode = {isDeleteMode} showDeleteModal={showDeleteModal} hideDeleteModal ={hideDeleteModal}></DeleteModal>
-      <DeleteToast></DeleteToast>
+        <DeleteModal
+          isDeleteMode={isDeleteMode}
+          showDeleteModal={showDeleteModal}
+          hideDeleteModal={hideDeleteModal}
+        ></DeleteModal>
+        <DeleteToast></DeleteToast>
       </div>
     </div>
   )
--- src/pages/Form/FormPages.jsx
+++ src/pages/Form/FormPages.jsx
@@ -1,35 +1,65 @@
-import React from "react";
-import styles from "./formPages.module.css";
+import React, { useState } from 'react'
+import styles from './formPages.module.css'
+
+const initMovie = {
+  name: 'defoult name',
+  img: 'https://pharmamex.com/images/default.png',
+  isFavorite: false,
+}
 
 export default function FormPages() {
+  const [newMovie, setNewMovie] = useState({ initMovie })
+
+  const handleOnChange = (e) => {
+    const nameMovie = e.target.nameMovie
+    const value = e.target.value
+    const temp = (newMovie[nameMovie] = value)
+    setNewMovie({ ...newMovie, temp })
+    console.log(newMovie)
+  }
+
+  const handleTitleOnChange = (e) => {
+    console.log(e.target.name)
+    const temp = { ...newMovie, title: e.target.value }
+    setNewMovie(temp)
+  }
+
+  const handleImgOnChange = (e) => {
+    console.log(e.target.value)
+    const temp = { ...newMovie, img: e.target.value }
+    setNewMovie(temp)
+  }
+
   return (
     <div>
       <form className={styles.form}>
         <div className={styles.formImgContainer}>
-          <img
-            className={styles.formImg}
-            src="https://pharmamex.com/images/default.png"
-            alt="movie"
-          />
+          <img className={styles.formImg} src={newMovie.img} alt="movie" />
         </div>
         <section className={styles.textInputsContainer}>
           <input
+            value={newMovie.name}
+            onChange={handleTitleOnChange}
             className={styles.nameInput}
             type="text"
             name="title"
             placeholder="movie name"
           />
+
           <textarea
+            value={newMovie.img}
+            onChange={handleImgOnChange}
             className={styles.urlInput}
             type="textarea"
             name="img"
             placeholder="introduce url"
           />
+
           <button className={styles.submitButton} type="submit">
             Crear
           </button>
         </section>
       </form>
     </div>
-  );
+  )
 }
