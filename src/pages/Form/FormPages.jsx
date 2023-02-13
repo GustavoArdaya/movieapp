8745bda272e98d6281092f23db07f694913dfc96
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-02-13 12:57:10 +0100

    changes

--- src/pages/Form/FormPages.jsx
+++ src/pages/Form/FormPages.jsx
@@ -1,31 +1,111 @@
-import React from "react";
-import styles from "./formPages.module.css";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import movieService from '../../apiService/movieService';
+import styles from './formPages.module.css';
+
+const initMovie = {
+  name: '',
+  img: 'https://pharmamex.com/images/default.png',
+  isFavorite: false,
+};
 
 export default function FormPages() {
+  const [newMovie, setNewMovie] = useState(initMovie);
+  const navigator = useNavigate();
+
+  const handleOnChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    newMovie[name] = value;
+    setNewMovie({ ...newMovie });
+    console.log(newMovie);
+  };
+
+  // const handleTitleOnChange = (e) => {
+  //   console.log(e.target.name);
+  //   const temp = { ...newMovie, name: e.target.value };
+  //   setNewMovie(temp);
+  // };
+
+  // const handleImgOnChange = (e) => {
+  //   console.log(e.target.value);
+  //   const temp = { ...newMovie, img: e.target.value };
+  //   setNewMovie(temp);
+  // };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await movieService.create(newMovie);
+    navigator('/');
+  };
+
   return (
     <div>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.formImgContainer}>
-          <img
-            className={styles.formImg}
-            src="https://pharmamex.com/images/default.png"
-            alt="movie"
-          />
+          <img className={styles.formImg} src={newMovie.img} alt='movie' />
         </div>
         <section className={styles.textInputsContainer}>
+          <textarea
+            value={newMovie.img}
+            onChange={handleOnChange}
+            className={styles.urlInput}
+            type='textarea'
+            name='img'
+            placeholder='introduce url'
+          />
           <input
+            value={newMovie.name}
+            onChange={handleOnChange}
             className={styles.nameInput}
-            type="text"
-            name="title"
-            placeholder="movie name"
+            type='text'
+            name='name'
+            placeholder='movie name'
+          />
+
+          <input
+            value={newMovie.director}
+            onChange={handleOnChange}
+            className={styles.nameInput}
+            type='text'
+            name='director'
+            placeholder='director'
+          />
+          <input
+            value={newMovie.year}
+            onChange={handleOnChange}
+            className={styles.nameInput}
+            type='text'
+            name='year'
+            placeholder='year'
+          />
+
+          <input
+            value={newMovie.genre}
+            onChange={handleOnChange}
+            className={styles.nameInput}
+            type='text'
+            name='genre'
+            placeholder='genre'
           />
           <textarea
+            value={newMovie.sinopsis}
+            onChange={handleOnChange}
             className={styles.urlInput}
-            type="textarea"
-            name="img"
-            placeholder="introduce url"
+            type='textarea'
+            name='sinopsis'
+            placeholder='sinopsis'
           />
-          <button className={styles.submitButton} type="submit">
+          {/* <input
+            value={newMovie.sinopsis}
+            onChange={handleOnChange}
+            className={styles.nameInput}
+            type='text'
+            name='sinopsis'
+            placeholder='Sinopsis'
+          /> */}
+
+          <button className={styles.submitButton} type='submit'>
             Crear
           </button>
         </section>
@@ -33,3 +113,38 @@ export default function FormPages() {
     </div>
   );
 }
+// import React from "react";
+// import styles from "./formPages.module.css";
+
+// export default function FormPages() {
+//   return (
+//     <div>
+//       <form className={styles.form}>
+//         <div className={styles.formImgContainer}>
+//           <img
+//             className={styles.formImg}
+//             src="https://pharmamex.com/images/default.png"
+//             alt="movie"
+//           />
+//         </div>
+//         <section className={styles.textInputsContainer}>
+//           <input
+//             className={styles.nameInput}
+//             type="text"
+//             name="title"
+//             placeholder="movie name"
+//           />
+//           <textarea
+//             className={styles.urlInput}
+//             type="textarea"
+//             name="img"
+//             placeholder="introduce url"
+//           />
+//           <button className={styles.submitButton} type="submit">
+//             Crear
+//           </button>
+//         </section>
+//       </form>
+//     </div>
+//   );
+// }
