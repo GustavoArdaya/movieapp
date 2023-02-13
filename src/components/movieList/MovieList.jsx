804accf79e39ca48bd18e56f1f69296fa02e38c3
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-02-13 11:15:03 +0100

    add function delete to api

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,63 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-import styles from './movieList.module.css'
-import movieService from '../../apiService/movieService'
-import MovieCard from '../MovieCard/MovieCard'
-import DeleteToast from '../DeleteToast/DeleteToast'
-import DeleteModal from '../DeleteModal/DeleteModal'
-import film_spinner from '../../assets/film_spinner.png'
+import styles from './movieList.module.css';
+import movieService from '../../apiService/movieService';
+import MovieCard from '../MovieCard/MovieCard';
+import DeleteToast from '../DeleteToast/DeleteToast';
+import DeleteModal from '../DeleteModal/DeleteModal';
+import film_spinner from '../../assets/film_spinner.png';
 
 export default function MovieList() {
-  const [movies, setMovies] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     movieService.getAll().then((data) => {
       setMovies(data);
       setIsLoading(false);
-    })
-  }, [])
+    });
+  }, []);
 
-  const deleteById = (idToDelete) => {
-    let newData = movies.filter((movie) => movie.id !== idToDelete)
-    setMovies(newData)
-  }
-  let idToDelete = "";
+  const deleteById = async (idToDelete) => {
+    await movieService.deleteById(idToDelete);
+    let newData = movies.filter((movie) => movie.id !== idToDelete);
+    setMovies(newData);
+  };
+  let idToDelete = '';
   const [isDeleteMode, setIsDeleteMode] = useState(false);
 
   const showDeleteModal = (id) => {
     setIsDeleteMode(true);
     idToDelete = id;
-    console.log(idToDelete)
-  }
+    console.log(idToDelete);
+  };
 
   const hideDeleteModal = () => {
     setIsDeleteMode(false);
-    
-  }
+  };
 
   return (
     <div>
-      <div className={ styles.spinnerContainer }>
-        {isLoading ?
-        <div> 
-          <img className={ styles.spinner } src={film_spinner}></img>
-          <p className={styles.loadingText}>Cargando...</p> 
-        </div>
-        :
-        ""}
+      <div className={styles.spinnerContainer}>
+        {isLoading ? (
+          <div>
+            <img className={styles.spinner} src={film_spinner}></img>
+            <p className={styles.loadingText}>Cargando...</p>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
       <div className={styles.movieList}>
         {movies.map((item) => (
-          <MovieCard key={item.id} item={item} showDeleteModal={showDeleteModal} />
+          <MovieCard
+            key={item.id}
+            item={item}
+            showDeleteModal={showDeleteModal}
+            deleteById={deleteById}
+          />
         ))}
       </div>
       <div className={styles.popups}>
-      <DeleteModal isDeleteMode = {isDeleteMode} showDeleteModal={showDeleteModal} hideDeleteModal ={hideDeleteModal}></DeleteModal>
-      <DeleteToast></DeleteToast>
+        {/* <DeleteModal
+          isDeleteMode={isDeleteMode}
+          showDeleteModal={showDeleteModal}
+          hideDeleteModal={hideDeleteModal}
+        ></DeleteModal>
+        <DeleteToast></DeleteToast> */}
       </div>
     </div>
-  )
+  );
 }
 
 // <div>
